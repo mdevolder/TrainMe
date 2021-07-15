84ea3754b4e21e9a4ad9bbeb149c9b2fc41990f2
mdevolder <matthew.devolder@gmail.com>
2021-07-15 16:58:53 -0500

    Added star ratings

--- schemas.js
+++ schemas.js
@@ -57,7 +57,7 @@ module.exports.trainerSchema = Joi.object({
 
 module.exports.reviewSchema = Joi.object({
     review: Joi.object({
-        rating: Joi.number().required().min(1).max(5),
+        rating: Joi.number().required().min(0).max(5),
         body: Joi.string().trim().required()
     }).required()
 });
