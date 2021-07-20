2a7218edccb04ad5dee79e2a92899f087c8575a6
mdevolder <matthew.devolder@gmail.com>
2021-07-20 13:25:42 -0500

    Added Joi html sanitization

--- schemas.js
+++ schemas.js
@@ -1,6 +1,27 @@
-const Joi = require('joi');
-//const Extension = require('joi-us-zipcode');
-//const Joi = BaseJoi.extend(Extension);
+const BaseJoi = require('joi');
+const sanitizeHtml = require('sanitize-html');
+
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{#label}} must not include HTML!'
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [],
+                    allowedAttributes: {},
+                });
+                if (clean !== value) return helpers.error('string.escapeHTML', { value })
+                return clean;
+            }
+        }
+    }
+});
+
+const Joi = BaseJoi.extend(extension)
 
 const states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
     'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
@@ -17,52 +38,60 @@ module.exports.trainerSchema = Joi.object({
         firstName: Joi
             .string()
             .trim()
-            .required(),
+            .required()
+            .escapeHTML(),
         lastName: Joi
             .string()
             .trim()
-            .required(),
+            .required()
+            .escapeHTML(),
         description: Joi
             .string()
             .trim()
-            .required(),
+            .required()
+            .escapeHTML(),
         street: Joi
             .string()
             .trim()
-            .required(),
+            .required()
+            .escapeHTML(),
         street2: Joi
             .string()
             .trim()
-            .allow('', null),
+            .allow('', null)
+            .escapeHTML(),
         city: Joi
             .string()
             .trim()
-            .required(),
+            .required()
+            .escapeHTML(),
         state: Joi
             .string()
             .trim()
             .required()
-            .valid(...states),
+            .valid(...states)
+            .escapeHTML(),
         zip: Joi
             .string()
             .trim()
             .regex(/^[0-9]{5}(?:-[0-9]{4})?$/)
-            .required(),
+            .required()
+            .escapeHTML(),
     }).required(),
     services: Joi
         .array()
-        .items(Joi.string()),
+        .items(Joi.string().escapeHTML()),
     serviceLocation: Joi
         .array()
-        .items(Joi.string()),
+        .items(Joi.string().escapeHTML()),
     certification: Joi
         .array()
-        .items(Joi.string())
+        .items(Joi.string().escapeHTML())
 });
 
 module.exports.reviewSchema = Joi.object({
     review: Joi.object({
         rating: Joi.number().required().min(0).max(5),
-        body: Joi.string().trim().required()
+        body: Joi.string().trim().required().escapeHTML()
     }).required()
 });
