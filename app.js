277013bf7d85d3cc2e11edf51760c5491fc310bf
mdevolder <matthew.devolder@gmail.com>
2021-07-20 21:57:06 -0500

    Fixed content security policy

--- app.js
+++ app.js
@@ -106,6 +106,7 @@ app.use(
             scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
             styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
             workerSrc: ["'self'", "blob:"],
+            childSrc: ["blob:"],
             objectSrc: [],
             imgSrc: [
                 "'self'",
